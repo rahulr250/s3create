70b53514d64a994050946f4197565648c48ca3f8
rahulr250 <39689167+rahulr250@users.noreply.github.com>
2023-01-31 02:12:15 +0530

    Update s3create.tf

--- s3create.tf
+++ s3create.tf
@@ -3,28 +3,30 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "4.52.0"
+      configuration_aliases = [ aws.west ]
     }
   }
 }
 
-provider "aws" {
-  region = "us-east-2"
+variable "bucket_name" {
+  type = string
+  description = "The name of the S3 bucket, must be globally unique"
 }
 
-provider "aws" {
-  alias = "west"
-  region = "us-west-2"
+resource "aws_s3_bucket" "my_bucket" {
+  provider = aws.west
+  bucket = var.bucket_name
 }
 
-module "create_s3" {
-  source = "./storage"
-  bucket_name = "rahul-demobucket2050"
-  providers = {
-    aws.west = aws.west
-   }
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  provider = aws.west
+  bucket                  = aws_s3_bucket.my_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 output "my_bucket_domain_name" {
-  value = module.create_s3.my_bucket_domain_name
+  value = aws_s3_bucket.my_bucket.bucket_domain_name
 }
-
