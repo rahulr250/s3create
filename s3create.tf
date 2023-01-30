8e0f8d07a14c9c764b01f44bf6d9ee76f355f858
rahulr250 <39689167+rahulr250@users.noreply.github.com>
2023-01-31 02:15:16 +0530

    Update s3create.tf

--- s3create.tf
+++ s3create.tf
@@ -1,25 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-      configuration_aliases = [ aws.west ]
-    }
-  }
-}
-
 variable "bucket_name" {
   type = string
   description = "The name of the S3 bucket, must be globally unique"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  provider = aws.west
   bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  provider = aws.west
   bucket                  = aws_s3_bucket.my_bucket.id
   block_public_acls       = true
   block_public_policy     = true
